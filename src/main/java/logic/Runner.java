90a94a1376caa53e2a3ddbb57bdc549d3b328068
nikit.cpp <nikit.cpp@yandex.ru>
2014-11-09 14:32:41 +0300

    commandline commons :)

--- src/main/java/logic/Runner.java
+++ src/main/java/logic/Runner.java
@@ -4,6 +4,8 @@ import net.lingala.zip4j.core.ZipFile;
 import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.ZipParameters;
 import net.lingala.zip4j.util.Zip4jConstants;
+import org.apache.commons.cli.*;
+import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -17,6 +19,7 @@ import java.util.List;
  */
 public class Runner {
     static String filesDir = "/home/nik/images";
+    public static Logger LOGGER = Logger.getLogger(Runner.class);
 
     /*public static void main(String... args) throws ZipException {
         ZipParameters parameters = new ZipParameters();
@@ -46,12 +49,40 @@ public class Runner {
     
     static String exit = "q";
 
+    static String INPUT_DIR_OPTION = "inputDir";
+
     /**
      *
      * @param args входная и выходная директории
-     * @throws IOException
+     *
      */
-    public static void main(String ...args) throws IOException{
+    public static void main(String... args){
+        Options options = new Options();
+
+        Option inputDirOption = OptionBuilder.withLongOpt(INPUT_DIR_OPTION)
+                .withArgName("input directory")
+                .hasArg()
+                .withDescription("Directory which contains input files. This directory will be scanned")
+                .isRequired()
+                .create("i");
+
+        HelpFormatter formatter = new HelpFormatter();
+
+        options.addOption(inputDirOption);
+        CommandLineParser cmdLinePosixParser = new PosixParser();// создаем Posix парсер
+
+        try {
+            CommandLine commandLine = cmdLinePosixParser.parse(options, args);// парсим командную строку
+            String inputDirValue = commandLine.getOptionValue(INPUT_DIR_OPTION);// если такая опция есть, то получаем переданные ей аргументы
+            System.out.println("We try to inputDir: " + inputDirValue);// выводим переданный логин
+        } catch (ParseException e) {
+            LOGGER.error("Error on process commandline args: " + e.getLocalizedMessage());
+            formatter.printHelp("fsArchiver", options);
+        }
+    }
+
+
+    /*public static void main(String ...args) throws IOException{
     	 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
     	 String isExit = "";
          FsPool fsPool = new FsPool();
@@ -66,6 +97,6 @@ public class Runner {
          } while (!isExit.equals(exit));
 
          bufferedReader.close();
-    }
+    }*/
     
 }
